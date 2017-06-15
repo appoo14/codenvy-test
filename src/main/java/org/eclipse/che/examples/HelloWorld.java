9a99a135a5cd253ed35f1be72e2b1efed0155cf4
abhyuday <appoo14@gmail.com>
2017-06-15 09:00:06 +0000

    Deleted extra html file

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -2,7 +2,8 @@ package org.eclipse.che.examples;
 
 public class HelloWorld {
     public static void main(String... argvs) {
-        String a = "Che";
-        System.out.println("Hello World " + a + "!");
+        String x = "Abhyuday Bharat";
+        
+        System.out.println("Hey "+x);
     }
 }
